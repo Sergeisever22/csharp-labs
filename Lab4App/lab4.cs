c5b462e9630c2d3fa50b91f6af306304128fafe7
Sever <s.severchenko2004@mail.com>
2025-10-02 02:27:37 +0300

    Lab4

---
+++ Lab4App/lab4.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+test1();
+test2();
+test3();
+
+void test1()
+{
+    int a = 5;
+    int b = 6;
+    a = b;
+    b = 7;
+    Console.WriteLine(a);
+    Console.WriteLine();
+}
+
+void test2()
+{
+    int a = 5;
+    int b = a + 6;
+    a = 7;
+    Console.WriteLine(b);
+    Console.WriteLine();
+}
+
+void test3()
+{
+    string a = "a";
+    string b = a;
+    a = "b";
+    Console.WriteLine(a);
+    Console.WriteLine(b);
+    Console.WriteLine();
+}
+
+//string a = 5;
--- lab2.cs
+++
@@ -1,5 +0,0 @@
-int a = 5;
-int b = 6;
-a = b;
-b = 7;
-Console.WriteLine(a);
