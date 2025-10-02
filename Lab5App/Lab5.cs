c2b56e1ed087a06c75ee4c276caaf20f13bd18bb
Sever <s.severchenko2004@mail.com>
2025-10-02 05:20:33 +0300

    Lab5

---
+++ Lab5App/Lab5.cs
@@ -0,0 +1,52 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Цены на товары
+
+Choices price = new()
+{
+    fanta = 12,
+    cola = 15,
+    cips = 22,
+};
+// Выбор клиентов
+
+Client client1 = new()
+{
+    FantaCount = 1,
+    ColaCount = 0,
+    CipsCount = 2,
+};
+
+Client client2 = new()
+{
+    FantaCount = 0,
+    ColaCount = 2,
+    CipsCount = 2,
+};
+
+//функция вычисляющая сумму цены всех покупок 
+static void ClientOrder(Choices price, Client client1, Client client2)
+{
+    int ClientOrder1 = client1.FantaCount * price.fanta + client1.ColaCount * price.cola + client1.CipsCount * price.cips;
+    int ClientOrder2 = client2.FantaCount * price.fanta + client2.ColaCount * price.cola + client2.CipsCount * price.cips;
+    Console.WriteLine(ClientOrder1);
+    Console.WriteLine();
+    Console.WriteLine(ClientOrder2);
+}
+
+ClientOrder( price,client1,client2);
+//Какие товары существуют
+struct Choices
+{
+    public int fanta;
+    public int cola;
+    public int cips;
+}
+
+// что может выбрать клиент
+struct Client
+{
+    public int FantaCount;
+    public int ColaCount;
+    public int CipsCount;
+}
